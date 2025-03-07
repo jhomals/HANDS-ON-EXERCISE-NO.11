66c7212ad8885dc88693408b511632dcdff9b6d4
jhomals <alsadojhomarie3@gmail.com>
2025-03-07 20:33:50 +0800

    jhom
    jhom

--- js-3/main.js
+++ js-3/main.js
@@ -1,19 +1,32 @@
 const elementTxt = document.getElementById("elements");
+let numInpt = document.getElementById("numInpt");
 const sumTxt = document.getElementById("sum");
-
-let arr = [];
+const highNum = document.getElementById("highNum");
+const lowNum = document.getElementById("lowNum");
 let sum = 0;
+let largest = 0;
+let lowest = 0;
+let arr = [];
 
-arr.push(23);
-arr.push(55);
-arr.push(10);
-arr.push(90);
-arr.push(18);
+function insertEl() {
+    arr.push(parseInt(numInpt.value));
+    let smallest = arr[0];
+    for (let i = 0; 1 < arr.length; i++) { 
+        sum += arr[i];  
+    }
 
-for(let i=0; i<arr.length; i++ ) {
-    sum += arr[i];
+    for(let i=0; i<arr.length; i++ ) {
+        if(arr[i] < smallest){
+            smallest = arr[i];
+        }
+        if(arr[i] < largest){
+            largest= arr[i];
+    }
 }
 
 elementTxt.innerHTML = arr.join("<br>");
 sumTxt.innerHTML = sum;
+highNum.innerHTML = largest;
+lowNum.innerHTML = lowest;
 console.log(arr);
+}
